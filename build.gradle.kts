782a355b363791038945d54995fdfa6ea7332462
Matthew Dilley <matthewrdilley@gmail.com>
2024-04-08 13:22:06 -0400

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,24 @@ plugins {
     id("java")
 }
 
-group = "org.game"
+group = "io.papermc.deimoscm"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
--- src/main/java/io/papermc/deimoscm/DeathBan.java
+++ src/main/java/io/papermc/deimoscm/DeathBan.java
@@ -1,4 +1,42 @@
 package io.papermc.deimoscm;
 
-public class DeathBan {
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+
+public class DeathBan extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
+        Bukkit.getServer().broadcastMessage(
+                event.getPlayer().getName()+" died at "+event.getPlayer().getLocation().getBlockX()+" "
+                        +event.getPlayer().getLocation().getBlockY()+" "+event.getPlayer().getLocation().getBlockZ()
+                        +" in "+getDimension(event)+", and has been banned for 24 hours!"
+        );
+        event.getPlayer().ban("Death-Banned! Come back tomorrow.", Duration.ofHours(24), "DeathBan", true);
+    }
+
+    private static @NotNull String getDimension(PlayerDeathEvent event) {
+        String dimension = "an unknown dimension";
+        if (event.getPlayer().getLocation().getWorld().getEnvironment() == World.Environment.NORMAL) {
+            dimension = "the overworld";
+        } else if (event.getPlayer().getLocation().getWorld().getEnvironment() == World.Environment.NETHER) {
+            dimension = "the nether";
+        } else if (event.getPlayer().getLocation().getWorld().getEnvironment() == World.Environment.THE_END) {
+            dimension = "the end";
+        }
+        return dimension;
+    }
 }
